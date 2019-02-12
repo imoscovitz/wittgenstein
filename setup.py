b5a7800375ad90bd3bc938df8cbf82e21a32b690
imoscovitz <ilan.moscovitz@gmail.com>
2019-02-12 14:34:31 -0800

    created description.rst, updated setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setuptools.setup(
   version = '0.1.0',
   license='MIT',
   description = 'Implementation of ruleset covering algorithms for explainable machine learning',
+  long_description=open('DESCRIPTION.rst').read(),
   author = 'Ilan Moscovitz',
   author_email = 'ilan.moscovitz@gmail.com',
   url = 'https://github.com/imoscovitz/ruleset',
-  keywords = ['Classification', 'Decision Rule', 'Machine Learning', 'Explainable Machine Learning'],
+  keywords = ['Classification', 'Decision Rule', 'Machine Learning', 'Explainable Machine Learning', 'Data Science'],
   packages=setuptools.find_packages(),
   install_requires=[
           'pandas',
