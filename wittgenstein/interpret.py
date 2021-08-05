4c065de34642be63d03b38c78667f7b62c091e11
imoscovitz <ilan.moscovitz@gmail.com>
2021-08-04 20:17:01 -0700

    add docstring, cleanup, and example for interpret

--- wittgenstein/interpret.py
+++ wittgenstein/interpret.py
@@ -13,7 +13,7 @@ def interpret_model(
     model_predict_function=None,
     score_function=score_accuracy,
 ):
-    """Interpret another model.
+    """Interpret another model using a wittgenstein interpreter as global surrogate.
 
         Parameters
         ----------
@@ -27,34 +27,19 @@ def interpret_model(
             if
         score_function : function, default=score_accuracy
             scoring function to evaluate how dutifully interpreter interpreted the model.
+
         Return
         ------
         tuple :
             interpreter fit to model,
-            scoring of how dutifully interpreter interpreted the model
+            scoring of how dutifully interpreter interpreted the model on training data
     """
     model_preds = utils.try_np_tonum(
         model_predict(X, model, model_predict_function=model_predict_function)
     )
     interpreter.fit(X, model_preds)
-    fidelity = score_fidelity(
-        X,
-        interpreter=interpreter,
-        model_preds=model_preds,
-        score_function=score_function,
-    )
     interpreter.base_model = model
-    return interpreter.ruleset_, fidelity
-
-
-def score_model(
-    X, y, model, score_function=score_accuracy, model_predict_function=None
-):
-    model_preds = utils.try_np_tonum(
-        model_predict(X, model=model, model_predict_function=model_predict_function)
-    )
-    return score_function(model_preds, y)
-
+    return interpreter.ruleset_
 
 def score_fidelity(
     X,
@@ -64,6 +49,27 @@ def score_fidelity(
     model_predict_function=None,
     score_function=score_accuracy,
 ):
+    """Score how faithfully interpreter represents model.
+
+    Parameters
+    ----------
+    X : DataFrame, numpy array, or other iterable
+        Test dataset with which to score the model.
+    interpreter : IREP or RIPPER object, default=RIPPER()
+        wittgenstein classifier to perform interpretation.
+    model : trained sklearn, keras, pytorch, or wittgenstein, etc. classifier, default=None
+        either model or model_preds are required
+    model_preds : iterable
+        model predictions on X, default=None
+    model_predict_function : function, default=None
+        model's prediction function. If None, will attempt to figure it out.
+    score_function : function or iterable of functions, default=score_accuracy
+        criteria to use for scoring fidelity
+
+    Returns
+    -------
+    score or list of scores"""
+
     if model is None and model_preds is None:
         raise ValueError(
             f"score_fidelity: You must pass a model or model predictions"
@@ -72,10 +78,15 @@ def score_fidelity(
         model_preds = utils.try_np_tonum(
             model_predict(X, model, model_predict_function=model_predict_function)
         )
-    return interpreter.score(X, model_preds, score_function)
+    if not hasattr(score_function, '__iter__'):
+        return interpreter.score(X, model_preds, score_function)
+    else:
+        return [interpreter.score(X, model_preds, func) for func in score_function]
 
 
 def model_predict(X, model, model_predict_function=None):
+    """Attempt to make predictions using model API"""
+
     if not model_predict_function:
         if _inpackage(model, "sklearn"):
             return _sklearn_predict(X, model)
@@ -96,12 +107,9 @@ def _sklearn_predict(X, model):
 
 
 def _keras_predict(X, model):
-    res = [p[0] for p in model.predict_classes(X)]
-    #print(f'result', res)
-    #return res
-    #return model(X)
-    return res
-    
+    return (model.predict(X) > 0.5).flatten()
+
+
 def _torch_predict(X, model):
     return model(X)
 
@@ -112,3 +120,11 @@ def _wittgenstein_predict(X, model):
 
 def _inpackage(model, str_):
     return str_ in str(type(model))
+
+def _score_model(
+    X, y, model, score_function=score_accuracy, model_predict_function=None
+):
+    model_preds = utils.try_np_tonum(
+        model_predict(X, model=model, model_predict_function=model_predict_function)
+    )
+    return score_function(model_preds, y)
