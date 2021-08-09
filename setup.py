dc53615de6f4aabd02393ac8052fe7852bcc7699
imoscovitz <ilan.moscovitz@gmail.com>
2021-08-08 23:29:58 -0700

    advance version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="wittgenstein",
-    version="0.2.3",
+    version="0.3.0",
     # license="MIT",
     description="Ruleset covering algorithms for explainable machine learning",
     long_description=open("README.md").read(),
